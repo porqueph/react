64141f4d30465986012738b16098d0707b4f87fc
Pedro Henrique Santos Fagundes <64497449+porqueph@users.noreply.github.com>
2023-07-11 22:17:43 -0300

    aaaa

---
+++ SecretWord/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <h2>Secret Word</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ SecretWord/src/data/word.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro:["Motor", "roda","capô","Pneu","Antena"],
+    fruta:["banana", "Maça","pera","Mamão","Laranja"],
+    corpo:["Braço", "Perna","Cérebro","pescoço","Olhos"],
+    computador:["Mouse", "Teclado","Monitor","Gabinete","Fone"],
+    programação:["Linguagem", "FrameWork","JavaScript","React","Java"],
+    alimento: ["arroz", "feijão","carne","leite","ovo"],
+};
