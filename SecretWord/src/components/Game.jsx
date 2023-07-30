0b84c21664b2f385a5d82b50528736d97b86bfbc
Pedro Henrique Santos Fagundes <64497449+porqueph@users.noreply.github.com>
2023-07-30 02:48:59 -0300

    CSS game

--- SecretWord/src/components/Game.jsx
+++ SecretWord/src/components/Game.jsx
@@ -2,9 +2,30 @@ import "./Game.css"
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-        <h1>game</h1>
-        <button onClick={verifyLetter}>finalizar</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+       <h1>adivinhe a palavra</h1>
+       <h3 className="tip">
+        dica sobre a palavra: <span>Dica...</span>
+       </h3>
+       <div className="wordContainer">
+         <span className="letter">A</span>
+         <span className="blankSquare"></span>
+       </div>
+       <div className="letterContainer">
+          <p>tente adivinhar uma letra da palavra:</p>
+          <form>
+            <input type="text" name="letter"  maxLength="1" required/>
+            <button>jogar</button>
+          </form>
+       </div>
+       <div className="wrongLettersContainer">
+        <p>letras utilizadas</p>
+        <span>a,</span>
+        <span>b,</span>
+       </div>
     </div>
   )
 }
