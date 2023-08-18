bf0311cf90937d6dd3d581486e4e54e2b1b1760d
Pedro Henrique Santos Fagundes <64497449+porqueph@users.noreply.github.com>
2023-08-18 17:02:05 -0300

    fetch de dados

--- HttpReq/src/App.jsx
+++ HttpReq/src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+    const url = "http://localhost:3000/products";
+
+    const [products, setProducts] = useState([]);
+
+    //chamada assync
+
+    useEffect(() => {
+     async function fetchData(){
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+     }
+     fetchData();
+    }, []);
+
+    console.log(products)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <h1>Lista de produtos</h1>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.name} - {product.price}</li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
     </>
   )
 }
